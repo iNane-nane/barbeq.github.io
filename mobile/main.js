ca2d782b93a93efbcf2bba6ad804bb3a12928e05
barbeq <dmitryblinov101@gmail.com>
2017-10-12 15:52:27 +0300

    mobile v0.0.2

--- mobile/main.js
+++ mobile/main.js
@@ -1,8 +1,10 @@
 'use strict';
 
-var swipeBody = new Hammer.Manager(document.body, {});
+var swipeBody = new Hammer.Manager(document.body, {}),
+    swipeTopbar = new Hammer.Manager(document.getElementById('topbar'), {touchAction: 'auto'});
 
 swipeBody.add( new Hammer.Pan({ direction: Hammer.DIRECTION_ALL, threshold: 0}) );
+swipeTopbar.add( new Hammer.Pan({ direction: Hammer.DIRECTION_HORIZONTAL, threshold: 0}) );
 
 swipeBody.on('panend', function(e) {
     if (e.additionalEvent === 'panright') {
@@ -11,6 +13,10 @@ swipeBody.on('panend', function(e) {
             left: '0'
         }, 500);
     }
+});
+
+swipeTopbar.on('panend', function (e) {
+    console.log(e)
     if (e.additionalEvent === 'panleft') {
         $('.topbar').animate({
             position: 'absolute',
